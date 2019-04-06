bb5b4501ad5db4ff1781e9e51ce4eca616f8faba
MaciejKut <maciej.kutzmann@gmail.com>
2019-04-06 09:44:38 +0200

    19-6 oragnizacja issues

---
+++ script-compiled.js
@@ -0,0 +1,10 @@
+'use strict';
+
+var _math = require('math');
+
+var lib = _interopRequireWildcard(_math);
+
+function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj; } else { var newObj = {}; if (obj != null) { for (var key in obj) { if (Object.prototype.hasOwnProperty.call(obj, key)) newObj[key] = obj[key]; } } newObj.default = obj; return newObj; } }
+
+console.log(lib.square(11)); // 121
+console.log(lib.diag(4, 3)); // 5
